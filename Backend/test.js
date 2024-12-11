f3bea1b10666a3ddb0f439e5ee553bbbad96f71b
SKY <listangco21@GMAIL.COM>
2024-12-11 15:16:13 +0800

    deleted some test logics

--- Backend/test.js
+++
@@ -1,4 +0,0 @@
-const membershipId = 1; // Replace with a valid membership ID
-generateQRCode(membershipId)
-    .then(path => console.log(`QR code generated at: ${path}`))
-    .catch(err => console.error('Error generating QR code:', err));
