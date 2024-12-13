4f09e6a4f0610f0dd456807b0f50295d55148080
Zeeee <129929654+Luxess24@users.noreply.github.com>
2024-12-13 13:30:36 +0800

    Adjust fonts and colors
    Changed Customer Trackin
    Income Summary
    Member Counting and SearchBar in Counter

--- frontend/src/components/admin/DatePicker.jsx
+++ frontend/src/components/admin/DatePicker.jsx
@@ -1,15 +1,19 @@
-
-import PropTypes from 'prop-types'; // Import PropTypes
+import PropTypes from 'prop-types';
 import '../../css/admin/DatePicker.css';
 
 function DatePicker({ setDate }) {
   const handleDateChange = (event) => {
     setDate(event.target.value); // Update the date in the parent component
+    event.target.blur();
+  };
+
+  const handleClick = (event) => {
+    event.target.showPicker(); // Show the date picker popup
   };
 
   return (
     <div className='datepicker'>
-      <input type="date" onChange={handleDateChange} />
+      <input type="date" onChange={handleDateChange} onClick={handleClick} />
     </div>
   );
 }
