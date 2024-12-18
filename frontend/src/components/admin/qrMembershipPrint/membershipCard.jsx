4567048ae2ad346a4983e885c3167d5a1b125791
Ervhyne(Catcher) <119420792+Ervhyne@users.noreply.github.com>
2024-12-19 00:12:20 +0800

    New Card Back Design and Fixes

--- frontend/src/components/admin/qrMembershipPrint/membershipCard.jsx
+++ frontend/src/components/admin/qrMembershipPrint/membershipCard.jsx
@@ -13,41 +13,41 @@ const MembershipCard = forwardRef(({ qrCodePath, member }, ref) => {
   };
 
   return (
-    <div ref={ref} className="membership-card">
-      <div className="card-header">
-        <div className="header-left">
-          <img src="/src/assets/Gym Icon.svg" alt="Gym Icon" className="gym-icon" 
-          style={{ width: '40px', height: '40px' }}
-          />
-        </div>
-        <div className="header-right">
-          <h1>CAVIN FITNESS GYM</h1>
-          <span>MEMBER CARD</span>
-        </div>
-      </div>
-      <div className="card-body">
-        <div className="left-side">
-          <p className="member-title">Member Name</p>
-          <p className="member-name">{member.name}</p>
-          <p>
-            <img src="/src/assets/Phone.svg" alt="Phone Icon" style={{ width: '20px', height: '20px' }} /> 0995 922 6260
-          </p>
-          <p>
-            <img src="/src/assets/Letter.svg" alt="Letter Icon" style={{ width: '20px', height: '20px' }} /> alvinagas73@gmail.com
-          </p>
-        </div>
-        <div className="right-side">
-          <div className="qr-container">
-            <img
-              src={qrCodePath}
-              alt="QR Code"
-              className="qr-code"
-              onError={handleImageError}
-              onLoad={handleImageLoad}
-            />
-            <span className="member-label">GYM MEMBER</span>
+    <div ref={ref} className="membership-card-container">
+      <div className="membership-card">
+        <div className="card-header">
+          <div className="header-right">
+            <h1>CAVIN FITNESS GYM</h1>
+            <span>MEMBER CARD</span>
           </div>
         </div>
+        <div className="card-body">
+          <div className="left-side">
+            <p className="member-title">Member Name</p>
+            <p className="member-name">{member.name}</p>
+            <p>
+              <img src="/src/assets/Phone.svg" alt="Phone Icon" style={{ width: '20px', height: '20px' }} /> 0995 922 6260
+            </p>
+            <p>
+              <img src="/src/assets/Letter.svg" alt="Letter Icon" style={{ width: '20px', height: '20px' }} /> alvinagas73@gmail.com
+            </p>
+          </div>
+          <div className="right-side">
+            <div className="qr-container">
+              <img
+                src={qrCodePath}
+                alt="QR Code"
+                className="qr-code"
+                onError={handleImageError}
+                onLoad={handleImageLoad}
+              />
+              <span className="member-label">CARD QR</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="membership-card-back">
+        {/* This is the backside of the card */}
       </div>
     </div>
   );
